2c3667738c1db4ec0790ed823e6797a31a3dd6e2
Sam Teel <sjkteel@hotmail.com>
2023-10-01 13:51:03 -0400

    added anchored images to work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import WorkDayScheduler from "../images/WorkDayScheduler.png";
+import EmployeeTrackerSQL from "../images/EmployeeManager.png";
+import SVGMaker from "../images/SVGMaker.png";
 
 
 function Work() {
     return (
         <div>
-
+            <section id="employeeTracker">
+                <a href="https://github.com/Teelsam/EmployeeTrackerSQL" ><img src={EmployeeTrackerSQL}></img></a>
+            </section>
+            <section id="workDayScheduler">
+                <a href="https://teelsam.github.io/Work-Day-Scheduler/"><img src={WorkDayScheduler}></img></a>
+            </section>
+            <section id="SVGMaker">
+                <a href="https://github.com/Teelsam/OOPSVGMaker"><img src={SVGMaker}></img></a>
+            </section>
         </div>
     )
 }
